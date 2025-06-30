c7cfa9fe18bc1c9c68d79b9ffd0069b14bda3361
ha0177 <214249506+Ha0177@users.noreply.github.com>
2025-06-30 17:16:50 +0300

    Add calculator exercise

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,28 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a - b;
+};
+const sum = function(array) {
+  return array.reduce((sum, current) => sum + current, 0);
 };
 
-const sum = function() {
-	
+const multiply = function(array) {
+  return array.reduce((total, current) => total * current);
 };
 
-const multiply = function() {
-
+const power = function(a, b) {
+  return a**b  
 };
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(a) {
+  let result = 1;
+  for (let i = a; i > 0; i--) {
+    result = result * i;
+  }
+  return result;
 };
 
 // Do not edit below this line
