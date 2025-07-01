df7bd932ff76099018610609768c8eb6b5076a54
ha0177 <214249506+Ha0177@users.noreply.github.com>
2025-07-01 10:53:13 +0300

    Add findTheOldest exercise

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,6 +1,23 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    let oldestPerson = null;
+    let oldestAge = 0;
+    for (let person of arr) {
+        let age;
+        let currentYear = new Date().getFullYear();
+        if (person.yearOfDeath === undefined) {
+            age = currentYear - person.yearOfBirth;
+        } else {
+            age = person.yearOfDeath - person.yearOfBirth;
+        }
+        if (age > oldestAge) {
+            oldestAge = age;
+            oldestPerson = person;
+        }
+    }
+    return oldestPerson;
 };
 
 // Do not edit below this line
 module.exports = findTheOldest;
+
+
