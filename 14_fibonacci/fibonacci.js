ef108f784b902ee6a3bc514d462ef15c5c8182c1
ha0177 <214249506+Ha0177@users.noreply.github.com>
2025-07-01 09:05:18 +0300

    Add fibonacci exercise

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function(a) {
+    if (a < 0) {
+        return "OOPS";
+    } else if (a == 0) {
+        return 0;
+    }
+    let previous = 0;
+    let current = 1;
+    for (let i = 2; i <= a; i++) {
+        let next = previous + current;
+        previous = current;
+        current = next;
+    }
+    return current;
 };
 
 // Do not edit below this line
